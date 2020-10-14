5008890b3b257d0353cd10962318d975eadb5fed
kasia.kordys <kasia.kordys@gmail.com>
2020-10-14 13:29:33 +0100

    add example controller with alternative views

---
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ViewResult Index()
+        {
+            return View("Homepage");
+            //return View("~/Index/Other/Index.cshtml");
+            //return View("Index", "AlternativeView")
+        }
+    }
+}
